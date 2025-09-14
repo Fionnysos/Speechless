4493e01288685e744f7329e6925165e59c7acecd
Fionn <fionn.zak@zak-digital.de>
2025-09-14 09:48:27 +0200

    created a responsemodel for different In- & Output (security)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Annotated, Any
+
+class UserIn(BaseModel):
+    username: Annotated[str, Field(min_length=3, max_length=32)]
+    password: Annotated[str, Field(min_length=8)]
+
+class UserOut(BaseModel):
+    username: str
+
 
 app = FastAPI()
 
@@ -6,6 +16,6 @@ app = FastAPI()
 async def root():
     return {"message": "ok"}
 
-@app.get("/docs")
-async def root():
-    return {"message": "ok"}
+@app.post("/register", response_model= UserOut)
+async def create_user(user: UserIn) -> Any:
+    return user
