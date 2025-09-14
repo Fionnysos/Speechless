908c2ed9770b6cec9cdf5b521b148e43c227663e
Fionn <fionn.zak@zak-digital.de>
2025-09-15 00:35:42 +0200

    built user creation while using db.py create_user function

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from typing import Annotated, Any
+from backend import auth, db
+from datetime import datetime, timezone
+
 
 class UserIn(BaseModel):
     username: Annotated[str, Field(min_length=3, max_length=32)]
@@ -18,4 +21,8 @@ async def root():
 
 @app.post("/register", response_model= UserOut)
 async def create_user(user: UserIn) -> Any:
+    hashed_password = auth.hash_password(user.password)
+    now_utc = datetime.now().replace(microsecond=0)
+    db.startup_database()
+    db.create_user_database(user.username, hashed_password, now_utc)
     return user
