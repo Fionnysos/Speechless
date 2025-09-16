af8b2678da1c664c1d62f184b77a45daa5feb913
Fionn <fionn.zak@zak-digital.de>
2025-09-16 15:25:52 +0200

    added create_session function and absolute path

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,15 @@
+from datetime import datetime, timedelta
+from backend import exceptions
 import sqlite3
+import secrets
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "db", "app.db")
 
 def startup_database():
-    conn = sqlite3.connect("../db/app.db")
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute("PRAGMA foreign_keys = ON;")
     return conn
@@ -13,3 +21,41 @@ def create_user_database(username, hashed_password, created_at):
     user_id = conn.cursor().lastrowid
     conn.close()
     return "User created: (?,?)", (user_id, username)
+
+def get_user_by_username(username):
+    conn = startup_database()
+    user = conn.cursor().execute("SELECT id, username, password_hash FROM users WHERE username = ?", (username,)).fetchone()
+    conn.close()
+    if user is None:
+        raise exceptions.UserNotFoundError("User not found")
+    else:
+        return user[1]
+
+def get_password_by_username(username):
+    conn = startup_database()
+    user = conn.cursor().execute("Select password_hash FROM users WHERE username = ?", (username,)).fetchone()
+    conn.close()
+    if user is None:
+        raise exceptions.UserNotFoundError("User not found")
+    else:
+        return user["password_hash"]
+
+def get_user_id_by_username(username):
+    conn = startup_database()
+    user = conn.cursor().execute("SELECT id FROM users WHERE username = ?", (username,)).fetchone()
+    conn.close()
+    if user is None:
+        raise exceptions.UserNotFoundError("User not found")
+    else:
+        return user[0]
+
+
+def create_session(username):
+    conn = startup_database()
+    token_str = secrets.token_urlsafe(64)
+    created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    expires_at = datetime.now() + timedelta(days=7)
+    conn.cursor().execute("INSERT INTO sessions (user_id, session_token, created_at, last_seen_at, expires_at, revoked) VALUES (?,?,?,?,?,?)", (get_user_id_by_username(username), token_str, created_at, created_at, expires_at, 0))
+    conn.commit()
+    conn.close()
+    return token_str
