9e85ed3c0d294e7332cdae835bc86eadc576cb0d
Fionn <fionn.zak@zak-digital.de>
2025-09-17 23:32:21 +0200

    changes because of combined functions

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ async def root():
 @app.post("/register", response_model= UserOut)
 async def create_user(user: UserIn) -> Any:
     hashed_password = auth.hash_password(user.password)
-    created_at = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
     db.startup_database()
     db.create_user_database(user.username, hashed_password, created_at)
     return user
@@ -29,13 +29,14 @@ async def create_user(user: UserIn) -> Any:
 @app.post("/login", response_model=UserOut)
 async def login(user: UserIn) -> Any:
     db.startup_database()
-    if user.username == db.get_user_by_username(user.username):
-        if auth.verify_password(user.password, db.get_password_by_username(user.username)):
-            db.create_session(user.username)
+    db_user = db.get_user(user.username, user.password)
+    if user.username == db_user["username"]:
+        if auth.verify_password(user.password, db_user[2]):
+            db.create_session(user.username, user.password)
             print("You are logged in!")
         else:
-            exceptions.InvalidPasswordError("Wrong password")
+           raise exceptions.InvalidPasswordError("Wrong password")
     else:
-        exceptions.UserNotFoundError("Username do not exist")
+        raise exceptions.UserNotFoundError("Username do not exist")
     return user
 
