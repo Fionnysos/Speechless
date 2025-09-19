537a367f4b07d96a25758954cfe2b72a8a2597f7
Fionn <fionn.zak@zak-digital.de>
2025-09-19 15:03:23 +0200

    added a exception

--- backend/auth.py
+++ backend/auth.py
@@ -1,9 +1,13 @@
 import bcrypt
+from backend import exceptions
 
 def hash_password(password: str) -> str:
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
-    return hashed_password.decode("utf-8")
+    if password == "":
+        raise exceptions.EmptyPasswordError
+    else:
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+        return hashed_password.decode("utf-8")
 
 def verify_password(password, hashed_password) -> bool:
     if bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8")):
