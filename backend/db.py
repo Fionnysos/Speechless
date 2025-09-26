d4bb413da01aee7d6095e9b28c77428ecd638b1a
Fionn <fionn.zak@zak-digital.de>
2025-09-26 10:37:55 +0200

    added tests

--- backend/db.py
+++ backend/db.py
@@ -10,6 +10,8 @@ import os.path
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "db", "app.db")
 
+
+
 def startup_database():
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
@@ -17,18 +19,14 @@ def startup_database():
     cursor.execute("PRAGMA foreign_keys = ON;")
     return conn
 
-def create_user_database(username, hashed_password, created_at):
-    conn = startup_database()
+def create_user_database(conn, username, hashed_password, created_at):
     conn.cursor().execute("INSERT INTO users (username,password_hash, created_at) VALUES (?, ?, ?)", (username, hashed_password, created_at))
     conn.commit()
     user_id = conn.cursor().lastrowid
-    conn.close()
     return "User created: (?,?)", (user_id, username)
 
-def get_user(username, password):
-    conn = startup_database()
+def get_user(conn, username, password):
     user = conn.cursor().execute("SELECT id, username, password_hash, created_at FROM users WHERE username = ?", (username,)).fetchone()
-    conn.close()
     if user is None:
         raise exceptions.UserNotFoundError("User not found")
     else:
@@ -38,8 +36,7 @@ def get_user(username, password):
             raise exceptions.InvalidPasswordError("Wrong Password!")
 
 
-def create_session(username, password):
-    conn = startup_database()
+def create_session(conn, username, password):
     db_user = get_user(username,password)
     token_str = secrets.token_urlsafe(64)
     created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -47,5 +44,4 @@ def create_session(username, password):
     expires_at = expires_at_raw.strftime("%Y-%m-%dT%H:%M:%SZ")
     conn.cursor().execute("INSERT INTO sessions (user_id, session_token, created_at, last_seen_at, expires_at) VALUES (?,?,?,?,?)", (db_user["id"], token_str, created_at, created_at, expires_at))
     conn.commit()
-    conn.close()
     return token_str
